309b04cd7de5955f434616f103cce5b2647a8fbe
edRiosJ <eduardo_silk@hotmail.com>
2022-10-17 13:16:39 -0500

    refactory modal proyects

---
+++ src/components/pures/cardProject/CardProject.jsx
@@ -0,0 +1,114 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BsGithub } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
+import CardIconModal from '../../containers/cardIconModal/CardIconModal';
+import Modal from '../modal/Modal';
+import './cardProject.css';
+
+function CardProject({
+  dataProject,
+  functionOpenModal,
+  valueActiveModal,
+  functionCloseModal,
+})
+{
+  const {
+    logoProject,
+    altImage,
+    linkDeployProject,
+    imageDeploy,
+    linkRepositoryProject,
+    descriptionProyect,
+    icons,
+  } = dataProject;
+  return (
+    <div className="cardProject">
+      <div className="imageProject" onClick={functionOpenModal}>
+        {
+          typeof logoProject === 'string' ? <img src={logoProject} alt={altImage} /> : logoProject
+        }
+      </div>
+      <Modal isOpen={valueActiveModal} closeModal={functionCloseModal}>
+        <div className="logoModal">
+          {
+            typeof logoProject === 'string' ? <img src={logoProject} alt={altImage} /> : logoProject
+          }
+        </div>
+
+        <div className="sectionModal">
+          <div className="leftPanel">
+            <div className="deployCardModal">
+              <a href={linkDeployProject} target="_blank" rel="noopener noreferrer">
+                <div className="imageDeployCard">
+                  <img src={imageDeploy} alt={altImage} />
+                </div>
+              </a>
+            </div>
+            <IconContext.Provider value={{ size: '10vw' }}>
+              <div className="repositoryCardModal">
+                <a href={linkRepositoryProject} target="_blank" rel="noopener noreferrer">
+                  <BsGithub />
+                </a>
+                <p>Repositorio</p>
+              </div>
+            </IconContext.Provider>
+          </div>
+
+          <div className="descriptionCardModal">
+            {descriptionProyect && descriptionProyect.map((el) => (
+              <p>
+                {el}
+                <br />
+                <br />
+              </p>
+            ))}
+          </div>
+        </div>
+
+        <div className="techCardModal">
+          {
+            icons && icons.map((icon) => (
+              <CardIconModal color={`${icon.color}`} name={`${icon.name}`} key={icon.name}>
+                {icon.icon}
+              </CardIconModal>
+            ))
+          }
+        </div>
+
+        <div className="videoModal">
+          <p>video</p>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+CardProject.propTypes = {
+  dataProject: PropTypes.objectOf(PropTypes.shape(
+    {
+      logoProject: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+      altImage: PropTypes.string.isRequired,
+      linkDeployProject: PropTypes.string.isRequired,
+      imageDeploy: PropTypes.string.isRequired,
+      linkRepositoryProject: PropTypes.string.isRequired,
+      descriptionProyect: PropTypes.arrayOf(PropTypes.string).isRequired,
+      icons: PropTypes.arrayOf(PropTypes.shape(
+        {
+          color: PropTypes.string.isRequired,
+          name: PropTypes.string.isRequired,
+          icon: PropTypes.node.isRequired,
+        },
+      )).isRequired,
+    },
+  )).isRequired,
+  functionOpenModal: PropTypes.func.isRequired,
+  valueActiveModal: PropTypes.bool.isRequired,
+  functionCloseModal: PropTypes.func.isRequired,
+};
+
+export default CardProject;
---
+++ src/utils/dataProjects.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  SiJavascript,
+  SiReact,
+  SiRedux,
+  SiReactrouter,
+  SiNodedotjs,
+  SiExpress,
+  SiSequelize,
+  SiPostgresql,
+} from 'react-icons/si';
+import logoYourCountry from '../img/logoYourCountry.svg';
+import yourCountry from '../img/yourCountry.png';
+
+const dataProjects = [
+  {
+    logoProject: logoYourCountry,
+    altImage: 'Your Country',
+    linkDeployProject: 'https://app-countries-omega.vercel.app',
+    imageDeploy: yourCountry,
+    linkRepositoryProject: 'https://github.com/edRiosJ/App-countries',
+    descriptionProyect: [
+      'Aplicación web con tema de países que realiza peticiones a una api y base de datos.',
+      '📌 Realizar búsquedas por nombre de país',
+      '📌 Filtrar países por continente y/o actividad turistica.',
+      '📌 Ordenar países de alfabeticamente o por número de habitantes tanto de forma ascendente como descendente.',
+      '📌 Crear actividades turisticas para uno o varios paises al mismo tiempo.',
+    ],
+    icons: [
+      { color: '#F7E018', name: 'JavaScript', icon: <SiJavascript /> },
+      { color: '#61DAFB', name: 'React', icon: <SiReact /> },
+      { color: '#764ABC', name: 'Redux', icon: <SiRedux /> },
+      { color: '#121212', name: 'ReactRouter', icon: <SiReactrouter /> },
+      { color: '#43853D', name: 'Node.js', icon: <SiNodedotjs /> },
+      { color: '#353535', name: 'Express', icon: <SiExpress /> },
+      { color: '#52B0E7', name: 'Sequelize', icon: <SiSequelize /> },
+      { color: '#336791', name: 'PostgresSQL', icon: <SiPostgresql /> },
+    ],
+  },
+  {
+    logoProject: <p>PadelApp</p>,
+    altImage: 'PadelApp',
+    linkDeployProject: 'https://padelapp.netlify.app',
+    imageDeploy: yourCountry,
+    linkRepositoryProject: 'https://github.com/pfpadelapp',
+    descriptionProyect: [
+      'Aplicación que tiene como objetivo simplificar la interacción entre el usuario que quiere reservar una cancha de padel y los propietarios de las mismas.',
+      '📌 Usuario puede buscar cancha por nombre, rango de precio, tipo de cancha.',
+      '📌 Usuario puede reservar una cancha y pagar la reservacion con PayPal.',
+      '📌 Usuario puede calificar y dejar comentarios de las canchas.',
+      '📌 Propietario puede agregar y eliminar sus canchas.',
+      '📌 Administrador puede banear y desbanear usuarios, propietarios y canchas.',
+      '📌 Y mas funcionalidades...',
+    ],
+    icons: [
+      { color: '#F7E018', name: 'JavaScript', icon: <SiJavascript /> },
+      { color: '#61DAFB', name: 'React', icon: <SiReact /> },
+      { color: '#121212', name: 'ReactRouter', icon: <SiReactrouter /> },
+      { color: '#43853D', name: 'Node.js', icon: <SiNodedotjs /> },
+      { color: '#353535', name: 'Express', icon: <SiExpress /> },
+    ],
+  },
+];
+
+export default dataProjects;
