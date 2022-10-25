e2f49f9e7f1f33e44765e7ff1d0e1b0657935ad1
edRiosJ <eduardo_silk@hotmail.com>
2022-10-24 22:12:14 -0500

    Contact section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutSection from '../../components/pures/aboutSection/AboutSection.jsx';
 import CertificationsSection from '../../components/pures/certificationsSection/CertificationsSection';
+import ContactSection from '../../components/pures/contactSection/ContactSection.jsx';
 import ExperienceSection from '../../components/pures/experienceSection/ExperienceSection';
 import ParallaxSection from '../../components/pures/parallaxSection/ParallaxSection';
 import ProjectsSection from '../../components/pures/projectsSection/ProjectsSection';
@@ -16,6 +17,7 @@ function Home()
       <ExperienceSection />
       <SkillsSection />
       <CertificationsSection />
+      <ContactSection />
     </>
   );
 }
