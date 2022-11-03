96176a6a943839e57a923401245253e0a77d69cb
edRiosJ <eduardo_silk@hotmail.com>
2022-11-02 22:30:00 -0600

    component footer has been relocation in the correct component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import AboutSection from '../../components/pures/aboutSection/AboutSection.jsx';
 import CertificationsSection from '../../components/pures/certificationsSection/CertificationsSection';
 import ContactSection from '../../components/pures/contactSection/ContactSection.jsx';
 import ExperienceSection from '../../components/pures/experienceSection/ExperienceSection';
-import Footer from '../../components/containers/footer/Footer.jsx';
 import ParallaxSection from '../../components/pures/parallaxSection/ParallaxSection';
 import ProjectsSection from '../../components/pures/projectsSection/ProjectsSection';
 import SkillsSection from '../../components/pures/skillsSection/SkillsSection';
@@ -19,7 +18,6 @@ function Home()
       <SkillsSection />
       <CertificationsSection />
       <ContactSection />
-      <Footer />
     </>
   );
 }
